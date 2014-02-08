a386f71eb082c25a64dfc505bfc6aa80cd93c771
vfjf <i.sahay@alumni.ubc.ca>
2014-02-08 14:50:48 -0800

    Added an empty file UndoablePacman that extends the MainUI & its test class

---
+++ src/main/java/org/jpacman/framework/ui/UndoablePacman.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package main.java.org.jpacman.framework.ui;
+
+import org.jpacman.framework.ui.MainUI;
+
+/**
+ * @author
+ */
+public class UndoablePacman extends MainUI {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// TODO:
+	public void undo() {
+	}
+
+}
