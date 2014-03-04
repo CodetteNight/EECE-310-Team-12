4d4151700a1c188b0f4c88809d0d1317af7d6153
vfjf <i.sahay@alumni.ubc.ca>
2014-03-04 04:00:24 -0800

    Began implementation of UndoableGame Deque of Moves. Moves are composed of Sprite and Tile pairs.

--- src/main/java/org/jpacman/framework/model/UndoableGame.java
+++ src/main/java/org/jpacman/framework/model/UndoableGame.java
@@ -1,11 +1,15 @@
 package main.java.org.jpacman.framework.model;
 
+import java.util.ArrayDeque;
+
 import org.jpacman.framework.model.Direction;
 import org.jpacman.framework.model.Game;
 import org.jpacman.framework.model.Ghost;
 
 public class UndoableGame extends Game {
 
+	private static ArrayDeque<Moves> moves = new ArrayDeque<Moves>();
+	
 	public void undo() {
 		return;
 
