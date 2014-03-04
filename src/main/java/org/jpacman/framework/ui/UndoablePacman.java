33c7cec6d3241c0b9f06e5d1918f3fb115804cf4
vfjf <i.sahay@alumni.ubc.ca>
2014-03-03 23:04:04 -0800

    Added an "Undo" button to the button panel. This button currently responds the same as the "Stop" button. Junit test continue to pass.

--- src/main/java/org/jpacman/framework/ui/UndoablePacman.java
+++ src/main/java/org/jpacman/framework/ui/UndoablePacman.java
@@ -8,6 +8,7 @@ import main.java.org.jpacman.framework.factory.UndoGameFactory;
 import org.jpacman.framework.factory.FactoryException;
 import org.jpacman.framework.model.IGameInteractor;
 import org.jpacman.framework.ui.MainUI;
+import org.jpacman.framework.ui.PacmanInteraction;
 
 /**
  * @author
@@ -19,19 +20,24 @@ public class UndoablePacman extends MainUI {
 	 */
 	private static final long serialVersionUID = 1L;
 
+
 	/**
-	 * The underlying game.
+	 * Mapping of UI events to model actions.
 	 */
-	private transient IGameInteractor theGame;
+	private transient PacmanInteraction pi;
+
+	/**
+	 * The main window components.
+	 */
+
+	private UndoButtonPanel buttonPanel;
+
 
 	// TODO:
 	public void undo() {
 	}
 
-	/*
-	 * public UndoablePacman() { setup(); } public void setup() { withFactory(new
-	 * UndoGameFactory()); }
-	 */
+
 	/**
 	 * @return The underlying game.
 	 */
@@ -63,4 +69,38 @@ public class UndoablePacman extends MainUI {
 		new UndoablePacman().main();
 	}
 
+	/**
+	 * Create the controllers and user interface elements.
+	 * 
+	 * @throws FactoryException
+	 *             If required resources can't be loaded.
+	 * @return The main UI object.
+	 */
+	@Override
+	public UndoablePacman initialize() throws FactoryException {
+		pi = new PacmanInteractionWithUndo();
+		buttonPanel = (UndoButtonPanel) new UndoButtonPanel().withParent(this).withInteractor(pi);
+
+		super.withModelInteractor(eventHandler());
+		super.withModelInteractor(pi).withButtonPanel(buttonPanel);
+		super.initialize();
+
+		setTitle("JPacman with Undo.");
+
+		return this;
+	}
+
+	/**
+	 * Create a new UI for the default board.
+	 */
+	/*
+	 * public UndoablePacman() { level = new Level(); }
+	 */
+	/**
+	 * @return The mapping between keyboard events and model events.
+	 */
+	@Override
+	public PacmanInteractionWithUndo eventHandler() {
+		return (PacmanInteractionWithUndo) pi;
+	}
 }
