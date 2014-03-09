f7d75900cf3def6dbc987a0c19d5104cbddccf4f
vfjf <i.sahay@alumni.ubc.ca>
2014-03-08 16:51:09 -0800

    Re-commit & push in case files were missed.

---
+++ src/main/java/org/jpacman/framework/model/IGameInteractorWithUndo.java
@@ -0,0 +1,18 @@
+package main.java.org.jpacman.framework.model;
+
+import org.jpacman.framework.model.IGameInteractor;
+
+/**
+ * Interface for manipulating the game.
+ * 
+ * @author Arie van Deursen, TU Delft, Jan 30, 2012
+ */
+public interface IGameInteractorWithUndo extends IGameInteractor {
+	
+	/**
+	 * Undo player moves by altering the model.
+	 */
+	void undo();
+
+}
+	
