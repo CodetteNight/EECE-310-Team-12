1582fc8dc709265ad6010a5579c796f94e4929e9
Sarah Louise Leong <slouise@live.com.my>
2014-03-23 21:37:56 -0700

    Implemented a share button that should post points on twitter. share() method currently does nothing significant except for printing a message on console

--- src/main/java/org/jpacman/framework/ui/UndoButtonPanel.java
+++ src/main/java/org/jpacman/framework/ui/UndoButtonPanel.java
@@ -2,10 +2,12 @@ package org.jpacman.framework.ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Observable;
 
 import javax.swing.JButton;
 
 import org.jpacman.framework.ui.ButtonPanel;
+import org.jpacman.framework.ui.PacmanInteraction.MatchState;
 
 public class UndoButtonPanel extends ButtonPanel {
 
@@ -17,7 +19,7 @@ public class UndoButtonPanel extends ButtonPanel {
 	private static final long serialVersionUID = 342724095694331357L;
 
 	private JButton undoButton;
-	private JButton postPointsButton;
+	private JButton shareButton;
 
 	/**
 	 * Actually create the buttons.
@@ -25,13 +27,13 @@ public class UndoButtonPanel extends ButtonPanel {
 	public void initialize() {
 		super.initialize();
 		undoButton = new JButton("Undo");
-		postPointsButton = new JButton("Share");
+		shareButton = new JButton("Share");
 		
 		initializeUndoButton();
-		initializePostPointsButton();
+		initializeShareButton();
 
 		addButton(undoButton);
-		addButton(postPointsButton);
+		addButton(shareButton);
 	}
 
 	/**
@@ -48,15 +50,20 @@ public class UndoButtonPanel extends ButtonPanel {
 		undoButton.setName("jpacman.undo");
 	}
 	
-	protected void initializePostPointsButton() {
-		postPointsButton.setEnabled(true);
-		postPointsButton.addActionListener(new ActionListener() {
+	protected void initializeShareButton() {
+		shareButton.setEnabled(false);
+		shareButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				undo();
+				share();
 			}
 		});
-		undoButton.setName("jpacman.postPoints");
+		undoButton.setName("jpacman.share");
+	}
+	
+	public void share(){
+		((IPacmanInteractionWithUndo) getPacmanInteractor()).share();
+		assert invariant();
 	}
 
 	public void undo() {
@@ -74,6 +81,21 @@ public class UndoButtonPanel extends ButtonPanel {
 	public void start() {
 		super.start();
 		undoButton.setEnabled(true);
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		super.update(o, arg);
+		enableShareButton();
+	}
+	
+	private void enableShareButton(){
+		MatchState s = ((PacmanInteractionWithUndo) getPacmanInteractor()).getCurrentState();
+		if(s == MatchState.LOST || s == MatchState.WON){
+			shareButton.setEnabled(true);
+		}
+		else
+			shareButton.setEnabled(false);
 	}
 
 }
