79d9f22d2bed4f8028765382d43e1f1db71485b7
vfjf <i.sahay@alumni.ubc.ca>
2014-03-24 23:09:43 -0700

    Corrected pom.xml file to create an executable jar with: mvn package

--- src/main/java/org/jpacman/framework/social/ApplicationAccess.java
+++ src/main/java/org/jpacman/framework/social/ApplicationAccess.java
@@ -6,16 +6,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Configuration
-@EnableAutoConfiguration
-@Import(TwitterConfig.class)
-@ComponentScan
-
-
 /**
  * Spring application to obtain tweets "#springframework".
  * From http://spring.io/guides/gs/accessing-twitter/
  */
+
+@Configuration
+@EnableAutoConfiguration
+@Import(TwitterConfig.class)
+@ComponentScan
 public class ApplicationAccess {
 
     /*
@@ -28,6 +27,7 @@ public class ApplicationAccess {
     
     public static void bootstrap(String[] args){
         SpringApplication.run(ApplicationAccess.class, args);
+        
     }
 
 } 
